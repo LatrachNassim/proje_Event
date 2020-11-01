d79117656d13d6d7463971f9154fe1d6d1e66704
Nassim <latrach.nassim@gmail.com>
2020-11-01 15:44:08 +0100

    Fin du css

--- js/liste.js
+++ js/liste.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
             let result = $('<article />', {}).appendTo('#results');
             let description = val.record.fields.description.substring(0, 140);
             let space = description.lastIndexOf(' ');
-            result.html('<img src="' + val.record.fields.cover_url + '"/>' + "<h2>" + val.record.fields.title + '</h2>' + '<p>' + val.record.fields.date_start + '</p>' + '<p>' + description.substring(0, space) + '... </p> <p>  <a href = "evenement.html?id='+ val.record.id +'"> voir plus </a> </p> <button>' + window.location.replace('favories.html') + '</button>');
+            result.html('<img src="' + val.record.fields.cover_url + '"/>' + "<h2>" + val.record.fields.title + '</h2>' + '<p>' + val.record.fields.date_start + '</p>' + '<p>' + description.substring(0, space) + '... </p> <p>  <a href = "evenement.html?id='+ val.record.id +'"> voir plus </a> </p> ');
 
 
         });
